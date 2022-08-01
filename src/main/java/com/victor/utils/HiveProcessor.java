2391f3ebc605b457bdc2b5718258afcbd2f79e73
Gerry <youyang.kou@relx.com>
2022-08-01 11:22:54 +0800

    add Hive Java Client HiveProcessor

---
+++ src/main/java/com/victor/utils/HiveProcessor.java
@@ -0,0 +1,43 @@
+package com.victor.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Gerry
+ * @date 2022-08-01
+ */
+public class HiveProcessor {
+
+    public static void main(String[] args) throws IOException {
+
+        String sql = "show tables; select * from db_real_sync_odps where tb='cdc_sync' and ds='20220720' limit 10";
+        List<String> command = new ArrayList<String>();
+
+        command.add("hive");
+        command.add("-e");
+        command.add(sql);
+
+        List<String> results = new ArrayList<String>();
+        ProcessBuilder hiveProcessBuilder = new ProcessBuilder(command);
+        Process hiveProcess = hiveProcessBuilder.start();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                hiveProcess.getInputStream()));
+
+        String data = null;
+        while ((data = br.readLine()) != null) {
+            results.add(data);
+        }
+
+        System.out.println("开始打印hive执行结果");
+        for (String result : results) {
+            System.out.println(result);
+        }
+
+    }
+
+}
